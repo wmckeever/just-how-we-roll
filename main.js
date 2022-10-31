b3ee55651bfed394b8441ba5354f41ab8dfe3111
wmckeever <wil.mckeever@gmail.com>
2022-10-31 12:00:18 -0400

    First push after xcode reinstall. Single d6 and Reset code started.

--- main.js
+++ main.js
@@ -22,24 +22,50 @@ const getRandomNumber = function(max) {
  * YOUR CODE BELOW *
  *******************/
 
+// Reset Button
+let resetButton = document.querySelector('#reset-button');
+
+//single d6 image
+let d6Image = document.querySelector('#d6-roll');
+let d6MeanDisplay = document.querySelector('#d6-rolls-mean');
+
+
 
 
 /*******************
  * EVENT LISTENERS *
  *******************/
-
+d6Image.addEventListener('click', function(){
+    console.log("D6 is working as intended!")
+   
+})
+resetButton.addEventListener('click', function(){
+    console.log("Reset button is working as intented!")
+    resetClicked();
+})
 
 
 
 /******************
  * RESET FUNCTION *
  ******************/
-
-
+function resetClicked () {
+    // empty global arrays
+    sixes = [];
+    doubleSixes = [];
+    twelves = [];
+    twenties = [];
+    
+    // reset images
+    d6Image.src = "./images/start/d6.png";
+    d6MeanDisplay.innerText = "NA";
+}
+// global reset function called when the page is loaded:
+resetClicked();
 
 /****************************
  * CLICK HANDLING FUNCTIONS *
-****************************/
+****************************/         
 
 
 
